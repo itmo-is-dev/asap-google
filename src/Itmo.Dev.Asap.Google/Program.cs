6bf80a32f99af4d20270eb4134d12ff7d7a76334
Roman Makarevich <remmybump@gmail.com>
2023-08-19 21:25:58 +0300

    feature: Added platform logging and YandexCloud

--- src/Itmo.Dev.Asap.Google/Program.cs
+++ src/Itmo.Dev.Asap.Google/Program.cs
@@ -1,3 +1,5 @@
+#pragma warning disable CA1506
+
 using Itmo.Dev.Asap.Google.Application.Extensions;
 using Itmo.Dev.Asap.Google.Application.Handlers.Extensions;
 using Itmo.Dev.Asap.Google.DataAccess.Extensions;
@@ -5,9 +7,13 @@ using Itmo.Dev.Asap.Google.Integrations.Github.Extensions;
 using Itmo.Dev.Asap.Google.Presentation.Grpc.Extensions;
 using Itmo.Dev.Asap.Google.Presentation.Kafka.Extensions;
 using Itmo.Dev.Asap.Google.Spreadsheets.Extensions;
+using Itmo.Dev.Platform.Logging.Extensions;
+using Itmo.Dev.Platform.YandexCloud.Extensions;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddUserSecrets<Program>();
+
+await builder.AddYandexCloudConfigurationAsync();
 
 builder.Services
     .AddGoogleApplication()
@@ -22,6 +28,9 @@ builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
 builder.Services.AddEndpointsApiExplorer();
 
+builder.AddPlatformSentry();
+builder.Host.AddPlatformSerilog(builder.Configuration);
+
 WebApplication app = builder.Build();
 
 await using (AsyncServiceScope scope = app.Services.CreateAsyncScope())
@@ -34,6 +43,7 @@ app.UseSwaggerUI();
 
 app.UseCors(o => o.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 app.UseRouting();
+app.UsePlatformSentryTracing(builder.Configuration);
 
 app.UseRpcPresentation();
 
