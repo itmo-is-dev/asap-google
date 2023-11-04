b0d9d4b3a5c20cf258cb9af0d9158d105658e564
ronimizy <ronimizy@outlook.com>
2023-11-04 23:35:10 +0300

    fix: registered table writer

--- src/Application/Itmo.Dev.Asap.Google.Application/Extensions/ServiceCollectionExtensions.cs
+++ src/Application/Itmo.Dev.Asap.Google.Application/Extensions/ServiceCollectionExtensions.cs
@@ -3,8 +3,10 @@ using FluentSpreadsheets.GoogleSheets.Extensions;
 using Itmo.Dev.Asap.Google.Application.Abstractions.Enrichment;
 using Itmo.Dev.Asap.Google.Application.Abstractions.Tables;
 using Itmo.Dev.Asap.Google.Application.Enrichment;
+using Itmo.Dev.Asap.Google.Application.Models.Tables.PartialPoints;
 using Itmo.Dev.Asap.Google.Application.Models.Tables.Points;
 using Itmo.Dev.Asap.Google.Application.Models.Tables.Queues;
+using Itmo.Dev.Asap.Google.Application.PartialPoints;
 using Itmo.Dev.Asap.Google.Application.Points;
 using Itmo.Dev.Asap.Google.Application.Providers;
 using Itmo.Dev.Asap.Google.Application.Queues;
@@ -19,7 +21,8 @@ public static class ServiceCollectionExtensions
     {
         collection
             .AddSingleton<ITableWriter<SubjectCoursePoints>, PointsTableWriter>()
-            .AddSingleton<ITableWriter<SubmissionQueue>, QueueTableWriter>();
+            .AddSingleton<ITableWriter<SubmissionQueue>, QueueTableWriter>()
+            .AddSingleton<ITableWriter<PartialSubjectCoursePoints>, PartialPointsTableWriter>();
 
         collection
             .AddFluentSpreadsheets()
